99500f3efe8ab6ec196a0cfe14ffab77f32fd23d
m1rageg <egor.sergei.demin153@gmail.com>
2024-01-01 19:50:32 +0200

    add check for status res

--- src/components/Request/Request.jsx
+++ src/components/Request/Request.jsx
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from 'react';
 
 const MyRequest = () => {
     const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
                 const jsonData = await response.json();
                 setData(jsonData);
             } catch (error) {
-                console.error('Error fetching data:', error);
+                console.error('Error fetching data:', error.message);
+                setError('Error fetching data. Please try again later.');
             }
         };
 
@@ -19,12 +26,16 @@ const MyRequest = () => {
 
     return (
         <div>
-            <h1>Request for data:</h1>
-            <ul>
-                {data.map(item => (
-                    <li key={item.id}>{item.title}</li>
-                ))}
-            </ul>
+            <h1>Request for data</h1>
+            {error ? (
+                <p>{error}</p>
+            ) : (
+                <ul>
+                    {data.map(item => (
+                        <li key={item.id}>{item.title}</li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
